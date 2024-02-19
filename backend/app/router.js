32ebb66d9cf359a220ab0d261021d3b93466b66b
Purwoceng <rahmadipurwo@gmail.com>
2024-02-19 17:09:17 +0700

    api tmdb

--- backend/app/router.js
+++ backend/app/router.js
@@ -1,8 +1,10 @@
-import {Router} from "express";
+import { Router } from "express";
 import user_routes from "./routes/user.js";
+import movie_routes from "./routes/movie.js";
 
 const router = Router();
 
 router.use(user_routes);
+router.use(movie_routes);
 
-export default router
+export default router;
