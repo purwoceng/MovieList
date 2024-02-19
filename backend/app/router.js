ca4a75225ccbf8a70208e53f338782741e387e08
Fayzaya G P <fayzayaganangputra20@gmail.com>
2024-02-19 22:24:29 +0700

    memperbaiki panggilan cinemas dari app.js ke router.js

--- backend/app/router.js
+++ backend/app/router.js
@@ -1,8 +1,10 @@
 import {Router} from "express";
 import user_routes from "./routes/user.js";
+import cinemas from "./app/routes/cinemas.js";
 
 const router = Router();
 
 router.use(user_routes);
+router.use(cinemas);
 
 export default router
