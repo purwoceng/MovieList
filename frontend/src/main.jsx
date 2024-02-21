4e9aa6fba9b3da4fc42312c20566370c3bd0b007
Purwoceng <rahmadipurwo@gmail.com>
2024-02-21 14:55:06 +0700

    redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
