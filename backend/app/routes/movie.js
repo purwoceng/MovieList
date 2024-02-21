227a796a60a06b07375e1e01ec8c6fbe1c6132b1
Fayzaya G P <fayzayaganangputra20@gmail.com>
2024-02-21 13:32:52 +0700

    memperbaiki struktur kode

--- backend/app/routes/movie.js
+++ backend/app/routes/movie.js
@@ -14,7 +14,6 @@ router.get("/movie", (req, res) => {
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTQxMDRmNTkyOWZlN2E1ODExOGMzMzI0NmM4YjA1ZiIsInN1YiI6IjY1Y2YwMTE2NmQxYmIyMDE3YjRjNzI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.03s-n84YsW0Md1LkWTb-ByxdAafxJxv9r4iR4SaVtfQ",
     },
   };
-
   fetch(url, options)
     .then((response) => response.json())
     .then((response) => {
