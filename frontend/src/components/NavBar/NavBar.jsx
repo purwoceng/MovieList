03087b1edbc9c2729b602f86f51bef6525aff5bf
alisy4f <alisy4f@gmail.com>
2024-02-21 16:38:30 +0700

    Add Sidebar component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, Drawer, Button, Avatar, useMediaQuery } from '@mui/material'
+import { AppBar, Toolbar, IconButton, Drawer, Button, Avatar, useMediaQuery, Box } from '@mui/material'
+import { useState } from 'react'
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
-import { useTheme } from '@mui/styles'
+import { useTheme } from '@mui/material/styles'
+import { Sidebar } from '..'
 
 const NavBar = () => {
+
+    const [mobileOpen, setMobileOpen] = useState(false)
     const theme = useTheme()
     const isAuthenticated = true
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+    const drawerWidth = 240
 
     return (
         <>
@@ -21,7 +25,7 @@ const NavBar = () => {
                         marginLeft: 0,
                         flexWrap: 'wrap'
                     }
-                    
+
 
                 }}>
                     {isMobile && (
@@ -29,7 +33,7 @@ const NavBar = () => {
                             color='inherit'
                             edge='start'
                             style={{ outline: 'none' }}
-                            onClick={() => {}}
+                            onClick={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
                             sx={{
                                 mr: theme.spacing(2),
                                 [theme.breakpoints.up('sm')]: {
@@ -42,23 +46,23 @@ const NavBar = () => {
                         </IconButton>
                     )}
 
-                    <IconButton color='inherit' sx={{ ml: 1 }} onClick={() => {}}>
+                    <IconButton color='inherit' sx={{ ml: 1 }} onClick={() => { }}>
                         {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
                     </IconButton>
                     {!isMobile && 'Search...'}
                     <div>
                         {isAuthenticated ? (
-                            <Button color='inherit' onClick={() => {}}>
+                            <Button color='inherit' onClick={() => { }}>
                                 Login &nbsp; <AccountCircle />
                             </Button>
                         ) : (
-                            <Button color='inherit' 
-                            component={Link} to={`/profile/:id`} className='' onClick={() => {}}>
+                            <Button color='inherit'
+                                component={Link} to={`/profile/:id`} style={{ '&: hover': { color: 'white !important', textDecoration: 'none' } }} onClick={() => { }}>
                                 {!isMobile && <>My Movies &nbsp;</>}
-                                <Avatar 
-                                style={{ width: 30, height: 30 }}
-                                alt='Profile' 
-                                src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F08%2F08%2F09%2F17%2Favatar-1577909_1280.png&f=1&nofb=1&ipt=e85ff4c93f0b3b4793757e9c98fdaefce573311f0250dbd5fcef4db083261d6f&ipo=images'
+                                <Avatar
+                                    style={{ width: 30, height: 30 }}
+                                    alt='Profile'
+                                    src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F08%2F08%2F09%2F17%2Favatar-1577909_1280.png&f=1&nofb=1&ipt=e85ff4c93f0b3b4793757e9c98fdaefce573311f0250dbd5fcef4db083261d6f&ipo=images'
                                 />
                             </Button>
                         )}
@@ -66,6 +70,29 @@ const NavBar = () => {
                     {isMobile && 'Search...'}
                 </Toolbar>
             </AppBar>
+
+            <div>
+                <Box component={'nav'} sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+                    {isMobile ? (
+                        <Drawer
+                            variant='temporary'
+                            anchor="right"
+                            open={mobileOpen}
+                            onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+                            classes={{ paper: { width: drawerWidth } }}
+                            ModalProps={{
+                                keepMounted: true
+                            }}
+                        >
+                            <Sidebar setMobileOpen={setMobileOpen} />
+                        </Drawer>
+                    ) : (
+                        <Drawer classes={{ paper: { width: drawerWidth } }} variant="permanent" open>
+                            <Sidebar setMobileOpen={setMobileOpen} />
+                        </Drawer>
+                    )}
+                </Box>
+            </div>
         </>
     )
 }
