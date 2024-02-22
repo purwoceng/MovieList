deffb0d45efcb527915077bd93be0b4024c63d67
alisy4f <alisy4f@gmail.com>
2024-02-22 12:56:01 +0700

    Fix MoviesList styling

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -7,28 +7,27 @@ const Movie = ({ movie, i }) => {
   const theme = useTheme();
 
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} sx={{ pading: "10px", textAlign: "center" }}>
       <Grow in key={i} timeout={(i + 1) * 250}>
-        <Link className={theme.links} to={`/movie/${movie.id}`}>
+        <Link style={{ textDecoration: 'none', fontWeight: "bolder", [theme.breakpoints.up('xs')]: { display: "flex", flexDirection: "column" }, '&:hover': {  cursor: 'pointer'} }} to={`/movie/${movie.id}`}>
           {movie.poster_path && (
             <img
               alt={movie.title}
-              className={theme.image}
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
               style={{
-                height: "250px",
-                width: "250px",
-                objectFit: "cover",
-                borderRadius: "10px",
+                borderRadius: "20px",
+                height: "300px",
+                marginBottom: "10px",
+                '&:hover': { transform: "scale(1.05)" },
               }}
             />
           )}
         </Link>
       </Grow>
-      <Typography variant="h5"> {movie.title} </Typography>
+      <Typography sx={{ color: theme.palette.text.primary, textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap", width: "100%", mt: "10px", mb: "0px", }} variant="h5"> {movie.title} </Typography>
       <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
-        <div>
-          <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+        <div >
+          <Rating   readOnly value={movie.vote_average / 2} precision={0.1} />
         </div>
       </Tooltip>
     </Grid>
