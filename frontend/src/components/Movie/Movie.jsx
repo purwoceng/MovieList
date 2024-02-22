512a3edec013fe4fd9723dac3015692e3da07e20
alisy4f <alisy4f@gmail.com>
2024-02-22 14:30:15 +0700

    -pull hover

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography, Grid, Grow, Tooltip, Rating } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
+import { Box } from "@mui/system";
 
 const Movie = ({ movie, i }) => {
   const theme = useTheme();
@@ -9,16 +10,17 @@ const Movie = ({ movie, i }) => {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} sx={{ pading: "10px", textAlign: "center" }}>
       <Grow in key={i} timeout={(i + 1) * 250}>
-        <Link style={{ textDecoration: 'none', fontWeight: "bolder", [theme.breakpoints.up('xs')]: { display: "flex", flexDirection: "column" }, '&:hover': {  cursor: 'pointer'} }} to={`/movie/${movie.id}`}>
+        <Link style={{ textDecoration: 'none', fontWeight: "bolder", [theme.breakpoints.up('xs')]: { display: "flex", flexDirection: "column" },  }} to={`/movie/${movie.id}`}>
           {movie.poster_path && (
-            <img
+            <Box
+              component="img"
               alt={movie.title}
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              style={{
+              sx={{
                 borderRadius: "20px",
                 height: "300px",
                 marginBottom: "10px",
-                '&:hover': { transform: "scale(1.05)" },
+                '&:hover': { transform: "scal(1.05)" },
               }}
             />
           )}
@@ -26,7 +28,7 @@ const Movie = ({ movie, i }) => {
       </Grow>
       <Typography sx={{ color: theme.palette.text.primary, textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap", width: "100%", mt: "10px", mb: "0px", }} variant="h5"> {movie.title} </Typography>
       <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
-        <div >
+        <div>
           <Rating   readOnly value={movie.vote_average / 2} precision={0.1} />
         </div>
       </Tooltip>
--- frontend/src/components/MovieList/MovieList.jsx
+++ frontend/src/components/MovieList/MovieList.jsx
@@ -7,7 +7,7 @@ const MovieList = ({ movies }) => {
   // const classes = useStyles(); // Uncomment if you plan to use useStyles
   const theme = useTheme();
   return (
-    <Grid container sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", overflow: "auto", [theme.breakpoints.down("sm")]: { justifyContent: "center" } }}>
+    <Grid container sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", ovflow: "hidden", [theme.breakpoints.down("sm")]: { justifyContent: "center" } }}>
       {movies.results.map((movie, i) => (
         <Movie key={i} movie={movie} i={i} />
       ))}
