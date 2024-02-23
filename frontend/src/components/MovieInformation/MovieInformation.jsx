8562c44e2b403c3c35f378cdd19298a815bbde1f
Ryan Gonz <wisnu.2722@students.amikom.ac.id>
2024-02-23 14:59:16 +0700

    movie detail

--- frontend/src/components/MovieInformation/MovieInformation.jsx
+++ frontend/src/components/MovieInformation/MovieInformation.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating } from '@mui/material'
+import {Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack,  } from '@mui/icons-material'
+import {Link, useParams} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { useGetMovieQuery } from '../../services/TMDB'
+import useStyles from './styles'
 
 const MovieInformation = () => {
   
+  const { id } = useParams()
+  const { data, isFetching, error } = useGetMovieQuery(id)
+  const classes = useStyles()
+
+  if(isFetching) {
+    return (<Box display="flex" justifyContent="center" alignItems="center">
+      <CircularProgress size="8rem" />
+    </Box>)
+  }
+
+  if(error) {
     return (
-    <div>MovieInformation</div>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    )
+  }
+
+    return (
+    <Grid container className={classes.containerSpaceAround}>
+      <Grid item sm={12} lg={4} xl={3}>
+        <img 
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container ></Grid>
+    </Grid>
+
   )
 }
 
--- frontend/src/services/TMDB.js
+++ frontend/src/services/TMDB.js
@@ -34,7 +34,11 @@ export const tmdbApi = createApi({
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`
       },
     }),
+    // Get Specific Movie
+    getMovie: builder.query({
+      query: (id) => `movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    })
   }),
 });
 
-export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
+export const { useGetMoviesQuery, useGetGenresQuery, useGetMovieQuery } = tmdbApi;
